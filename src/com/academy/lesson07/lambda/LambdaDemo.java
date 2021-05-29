bf9730ffebaa021830f6a6d6f7ade17c16c814db
af_oleg <oleg.kh81@gmail.com>
2021-05-29 11:22:58 +0300

    lesson 07 -> lambda

--- src/com/academy/lesson07/lambda/LambdaDemo.java
+++ src/com/academy/lesson07/lambda/LambdaDemo.java
@@ -26,6 +26,30 @@ public class LambdaDemo {
 
         Boxer fighter5 = ()->System.out.println("New boxing technique (lambda)");
         fighter5.boxing();
+
+        Operation calcSum = (n1, n2)-> n1 + n2; // способ реализации Operation -> сумма
+        Operation calcDiff = (n1, n2)-> n1 - n2;
+        Operation calcMult = (n1, n2)-> n1 * n2;
+
+        System.out.println(calcSum.calc(3, 4));
+        System.out.println(calcDiff.calc(3, 4));
+        System.out.println(calcMult.calc(3, 4));
+
+        Operation sumImpl = new OperationSumImpl(); //длинный способ реализации Operation -> сумма
+        System.out.println(sumImpl.calc(1, 2));
+
+        Operation calcSumExt = (n1, n2) -> {
+            System.out.println("Вычисляю сумма двух чисел");
+            return n1 + n2;
+        };
+
+        System.out.println(calcSumExt.calc(3, 2));
+
+        Operation calcSumUsingMethod1 = (n1, n2)->sum(n1, n2);
+        System.out.println(calcSumUsingMethod1.calc(1, 3));
+
+        Operation calcSumUsingMethod2 = LambdaDemo::sum;
+        System.out.println(calcSumUsingMethod2.calc(1, 3));
     }
 
     public static int sum(int number1, int number2) {
