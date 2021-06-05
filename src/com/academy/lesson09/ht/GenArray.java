3fa6e75d935b4c99d5fd3263cb3b3e1ec6ffe248
af_oleg <oleg.kh81@gmail.com>
2021-06-05 11:23:53 +0300

    lesson 09 -> ht

--- src/com/academy/lesson09/ht/GenArray.java
+++ src/com/academy/lesson09/ht/GenArray.java
@@ -2,4 +2,12 @@ package com.academy.lesson09.ht;
 
 public class GenArray<T> {
     private T[] array;
+
+    private T get(int index){
+
+    }
+
+    void set(int index, T element) {
+
+    }
 }
---
+++ src/com/academy/lesson09/ht/GenArrayDemo.java
@@ -0,0 +1,12 @@
+package com.academy.lesson09.ht;
+
+public class GenArrayDemo {
+    public static void main(String[] args) {
+        // Создать класс GenArray<String> размера 10
+        // Записать 2-ой элемент "hello"
+        // Прочитать этот элемент
+
+        // Записать 11-ый элемент "world"
+        // Получить exception
+    }
+}
