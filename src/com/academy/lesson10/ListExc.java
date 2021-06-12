fad2d64298fcf98b8051e9664d52a23a29e68ae3
af_oleg <oleg.kh81@gmail.com>
2021-06-12 10:25:51 +0300

    lesson 10 -> ht

--- src/com/academy/lesson10/ListExc.java
+++ src/com/academy/lesson10/ListExc.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class ListExc {
     public static void main(String[] args) {
-        String[] names = {"Peter", "Helen", "Andry", "Abdel", "Kate", "Veronica", "Leonid", "Alex", "max"};
+        String[] names = {"Peter", "Helen", "Andry", "Abdel",
+                "Kate", "Veronica", "Leonid", "Alex", "max"};
 
         // 1 способ из массива коллекцию
         List<String> listNames = new ArrayList<>();
@@ -32,5 +33,19 @@ public class ListExc {
         listNames.sort((o1, o2)-> o1.compareTo(o2));
         System.out.println(listNames);
 
+        System.out.println(listNames);
+        // 1 способ
+        System.out.println("1 способ");
+        for(int i = 0; i < listNames.size(); i++) {
+            if(listNames.get(i).startsWith("A") || listNames.get(i).startsWith("a")) {
+                System.out.println(listNames.get(i));
+            }
+        }
+
+        // 2 способ через Stream API
+        System.out.println("2 способ");
+        listNames.stream()
+                .filter(e->e.startsWith("A"))
+                .forEach(System.out::println);
     }
 }
